4e2651ce63965a1042c57ea3b7b170e8ebd31884
TipzTeam <rcheung844@gmail.com>
2020-08-21 22:08:33 +0800

    test: add a simple Hello myself

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
-import telethon
+from telethon import TelegramClient
 
-print(telethon.__version__)
+api_id = api id here
+api_hash = 'api hash here'
+
+with TelegramClient('anon', api_id, api_hash) as client:
+    client.loop.run_until_complete(client.send_message('me', 'Hello, myself!'))
